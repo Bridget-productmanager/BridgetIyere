00a4f5bb3ef954f34102ab0dd228cc09adb72c93
Bridget-productmanager <60605611+Bridget-productmanager@users.noreply.github.com>
2020-03-27 16:09:51 +0100

    Add files via upload

---
+++ Javascript Task.js
@@ -0,0 +1,8 @@
+let name = 'Bridget Iyere';
+let courses = ['HTML','CSS','JAVASCRIPT','DESIGN'];
+
+console.log('My Name is ', name,' and my courses are ', courses.join());
+if((courses.length % 2) === 0){
+for (let i = 1; i<=200; i++) 
+if (i%2 === 0) console.log(i);
+};
